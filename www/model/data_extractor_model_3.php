3b9c98edf35141c849feae7f3eb9ecfee4534f2c
César Castillo López <30807253+cesarcas1994@users.noreply.github.com>
2020-11-24 14:22:47 -0600

    compress data extractor file

--- www/model/data_extractor_model_3.php
+++ www/model/data_extractor_model_3.php
@@ -1,14 +1,1 @@
-<?php
-
-/**
- * data_extractor_model_3
- */
-//category with 2000 items
-//$params = "category_id=MLM158032";
-
-//category with 16136 items
-$params = "category_id=MLM73302";
-
-//phpinfo();
-
-$meli -> data_extractor($params);
+<?php$params="category_id=MLM73302";$meli->data_extractor($params);
